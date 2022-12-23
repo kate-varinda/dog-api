88c09da2fd5ad99959a5b88b4cc3c9c84553d63e
Kate <varindakate@gmail.com>
2022-12-23 00:31:11 -0800

    finished dog-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [imageUrl, setImageUrl] = useState(
+    "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg"
+  );
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const getRandomImage = () => {
+    axios
+      .get("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        setImageUrl(response.data.message);
+      })
+      .catch((error) => {
+        setErrorMessage(<section>{error.response.data.message}</section>);
+      });
+  };
+
+  const onButtonClick = () => {
+    getRandomImage();
+  };
+
+  useEffect(() => getRandomImage, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Dog Log</h1>
+      {errorMessage}
+      <div>
+        <button onClick={onButtonClick}>Get New Random Dog</button>
+        <img src={imageUrl} alt="A random dog" />
+      </div>
     </div>
   );
 }
